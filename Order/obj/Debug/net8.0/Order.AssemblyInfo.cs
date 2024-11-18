84ba9b619473cc707f35c81e46da4f1c7443c429
mLq <syncofficial@yandex.ru>
2024-11-18 20:15:32 +0500

    fixing dependencies

--- Order/obj/Debug/net8.0/Order.AssemblyInfo.cs
+++ Order/obj/Debug/net8.0/Order.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Order")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+484502f84f9bb98bc565df4557d5aa3d2b72ede1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8070084d96295533f73e47bb60768d947fd5946")]
 [assembly: System.Reflection.AssemblyProductAttribute("Order")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Order")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
